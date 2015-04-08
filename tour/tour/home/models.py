0a6e4037255a7748f653814bfea104e866e59325
narwally <c.bowman1711@gmail.com>
2015-04-08 12:23:46 -0400

    Create TourStop model

--- tour/tour/home/models.py
+++ tour/tour/home/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.conf import settings
 
 
-class Stop(models.Model):
+class TourStop(models.Model):
 	"""Stop Model"""
 	title = models.CharField(_('title'), max_length=100)
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.title
