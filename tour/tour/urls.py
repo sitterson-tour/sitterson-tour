1efc9c487aef0fe19a0a20f78c72ba5b7dec0f97
narwally <c.bowman1711@gmail.com>
2015-04-08 21:26:51 -0400

    Add contact.html and tour-stops.html to url.py under /contact/ and /stops/

--- tour/tour/urls.py
+++ tour/tour/urls.py
@@ -8,6 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', TemplateView.as_view(template_name="home.html")),
+	url(r'^contact/', TemplateView.as_view(template_name="contact.html")),
+	url(r'^stops/', TemplateView.as_view(template_name="tour-stops.html")),
 	url(r'^admin/', include(admin.site.urls)),
+
 
 )
