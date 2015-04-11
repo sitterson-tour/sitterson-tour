35367914b0d8afa7ffa435bb02cc3470ecc3dad3
narwally <c.bowman1711@gmail.com>
2015-04-10 23:14:53 -0400

    Add more detail to TourStop model

--- tour/tour/models.py
+++ tour/tour/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import slugify
 
 
 class TourStop(models.Model):
     """Stop Model"""
     title = models.CharField( max_length=100)
+    slug = models.SlugField( max_length=100)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=True)
     
     def __unicode__(self):
 		return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Stop, self).save(*args, **kwargs)
