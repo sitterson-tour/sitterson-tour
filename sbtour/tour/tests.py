65fdeaf9dd607e0e0a57d1ea627a8671acd31149
narwally <c.bowman1711@gmail.com>
2015-05-02 22:06:11 -0400

    get tests running

--- sbtour/tour/tests.py
+++ sbtour/tour/tests.py
@@ -1,28 +1,21 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from django.test import RequestFactory
+from .views import tour_home_view, tour_stop_view
 
 
 class HomePageViewTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = RequestFactory().get('/')
-        view = HomePageView.as_view(template_name='index.html')
-        
-        response = view(request)
-        
+        response = tour_home_view(request)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template_name[0], 'index.html')
 
 
 class TourStopViewTest(TestCase):
 
     def test_tour_stop_page(self):
-        request = RequestFactory().get('/stops/')
-        view = TourStopView.as_view(template_name='stop.html')
-
-        response = view(request)
-
+        request = RequestFactory().get('/stop/')
+        response = tour_stop_view(request, 'pixel-planes')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template_name[0], 'stop.html')
 
